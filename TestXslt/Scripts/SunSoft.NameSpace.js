4c1d9b59f61171bef9fee88e887683a261eed514
greatbody <wedown@163.com>
2014-08-13 23:31:44 +0800

    new

---
+++ TestXslt/Scripts/SunSoft.NameSpace.js
@@ -0,0 +1,12 @@
+﻿var SunSoft = SunSoft || {};
+SunSoft.Namespace = SunSoft.Namespace || {};
+(function() {
+    SunSoft.Namespace.AddObjectByName = function(sNameSpace, sFunc) {
+        var sCommand = "(function(){var " + sNameSpace + "=" + sNameSpace + "||{};";
+        sCommand = sCommand + sNameSpace + "." + sFunc + "=" + sFunc + ";return " + sNameSpace + ";})()";
+        eval(sCommand);
+    };
+    function createComm(namespace,sfunc) {
+        
+    }
+})();
